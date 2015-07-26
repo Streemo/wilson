c2944405199eb35dec090a29960634afba591eca
Jay Patel <jay@Jays-MacBook-Pro.local>
2015-07-26 19:16:28 -0400

    initial

---
+++ package.js
@@ -0,0 +1,15 @@
+Package.describe({
+  name: 'streemo:wilson',
+  version: '0.0.1',
+  summary: 'The Wilson algorithm. Score/rank posts, detect spam, make decisions, etc.',
+  git: 'https://github.com/Streemo/wilson.git',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles('lib/server/maclaurin.js','server');
+  api.addFiles('lib/server/wilson.js','server');
+  api.addFiles('lib/server/init.js','server');
+  api.export(['Wilson']);
+});
